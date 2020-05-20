721bb4767fcc4e91d2c1c1de68445f65880735bc
Hung Huynh <kenhuynh257@gmail.com>
2020-05-20 13:02:37 -0700

    fixing name space from HW1 to RedhawksTradingPost

--- RedhawksTradingPost/Pages/Privacy.cshtml.cs
+++ RedhawksTradingPost/Pages/Privacy.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace HW1.Pages
+namespace RedhawksTradingPost.Pages
 {
     public class PrivacyModel : PageModel
     {
