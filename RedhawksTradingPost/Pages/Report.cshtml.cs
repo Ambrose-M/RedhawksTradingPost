8c2c54fcf6c1d696cf36de2b8a8a4beae4cba7b7
ambrose-m <ambrose907@gmail.com>
2020-05-24 11:14:43 -0700

    New page for reporting items

---
+++ RedhawksTradingPost/Pages/Report.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RedhawksTradingPost.Pages
+{
+    public class ReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
