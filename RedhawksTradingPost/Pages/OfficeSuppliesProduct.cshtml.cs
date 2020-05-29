11b0de5a7418cd1d5532484bd781deeeaa885cc1
Hung Huynh <huynhh1@seattleu.edu>
2020-05-29 10:38:43 -0700

    initiate officesupplies product page with cs code
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/1CyANB-PnUKcCRqyD-ZuLWQAJPyF?Type=TaskLink&Channel=Link&CreatedTime=637263707106100000

--- RedhawksTradingPost/Pages/OfficeSuppliesProduct.cshtml.cs
+++ RedhawksTradingPost/Pages/OfficeSuppliesProduct.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RedhawksTradingPost.Models;
+using RedhawksTradingPost.Services;
 
 namespace RedhawksTradingPost.Pages
 {
+
     public class OfficeSuppliesProductModel : PageModel
     {
+        private readonly ILogger<OfficeSuppliesProductModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+        public OfficeSuppliesProductModel(
+            ILogger<OfficeSuppliesProductModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
         public void OnGet()
         {
-
+            Products = ProductService.GetOfficeSuppliesProducts();
         }
     }
 }
