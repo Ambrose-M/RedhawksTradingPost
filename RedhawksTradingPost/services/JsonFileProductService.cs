38153831e0fc1bcd6d504e11ceb2c3fb54c8892b
Hung Huynh <huynhh1@seattleu.edu>
2020-05-29 10:28:52 -0700

    Add comment for jsonfileproductService.cs

--- RedhawksTradingPost/services/JsonFileProductService.cs
+++ RedhawksTradingPost/services/JsonFileProductService.cs
@@ -8,14 +8,24 @@ using Microsoft.AspNetCore.Hosting;
 namespace RedhawksTradingPost.Services
 {
     public class JsonFileProductService
-    {
+    {   
+        /// <summary>
+        /// Path to webhostEnvirommmnet
+        /// </summary>
+        /// <param name="webHostEnvironment"></param>
         public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
 
+        /// <summary>
+        /// function to get webhostenviroment
+        /// </summary>
         public IWebHostEnvironment WebHostEnvironment { get; }
 
+        /// <summary>
+        /// Path to product.json
+        /// </summary>
         private string JsonFileName
         {
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
@@ -29,6 +39,10 @@ namespace RedhawksTradingPost.Services
             get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "OfficeSuppliesProduct.json"); }
         }
 
+        /// <summary>
+        /// get all the product from product.json
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Product> GetProducts()
         {
             using (var jsonFileReader = File.OpenText(JsonFileName))
@@ -42,7 +56,7 @@ namespace RedhawksTradingPost.Services
         }
 
         /// <summary>
-        /// Function read data from office suppliesproduct
+        /// Function read data from officesuppliesproduct.json
         /// </summary>
         /// <returns></returns>
         public IEnumerable<Product> GetOfficeSuppliesProducts()
