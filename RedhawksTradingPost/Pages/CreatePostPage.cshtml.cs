a8a5c8b54913112288293ea214be9388614ecdb7
nwu124 <55820966+nwu124@users.noreply.github.com>
2020-05-31 13:48:12 -0700

    Added json services to CreatePostPage.cshtml.cs

--- RedhawksTradingPost/Pages/CreatePostPage.cshtml.cs
+++ RedhawksTradingPost/Pages/CreatePostPage.cshtml.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RedhawksTradingPost.Models;
+using RedhawksTradingPost.Services;
 
 namespace RedhawksTradingPost.Pages
 {
     public class CreatePostPageModel : PageModel
     {
+        private readonly ILogger<UserPageModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+        public CreatePostPageModel(
+            ILogger<UserPageModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
