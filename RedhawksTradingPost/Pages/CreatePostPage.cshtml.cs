aa52dc4fbd24aacc6e7bdfb01734e85675163fe1
nwu124 <55820966+nwu124@users.noreply.github.com>
2020-06-01 14:52:45 -0700

    Added comments to CreatePostPage.cshtml.cs
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/XOqDjkDEjUGI4CJh4dPq12QAOvzY?Type=TaskLink&Channel=Link&CreatedTime=637266451587420000

--- RedhawksTradingPost/Pages/CreatePostPage.cshtml.cs
+++ RedhawksTradingPost/Pages/CreatePostPage.cshtml.cs
@@ -12,9 +12,13 @@ namespace RedhawksTradingPost.Pages
 {
     public class CreatePostPageModel : PageModel
     {
+        // ILogger
         private readonly ILogger<UserPageModel> _logger;
+        // JsonFileProductService for interacting with products.json
         public JsonFileProductService ProductService;
+        // Product list for us to populate, relevant get/set functions
         public IEnumerable<Product> Products { get; private set; }
+        // Creates a logger and JsonFileProductService for us to use
         public CreatePostPageModel(
             ILogger<UserPageModel> logger,
             JsonFileProductService productService)
@@ -22,8 +26,10 @@ namespace RedhawksTradingPost.Pages
             _logger = logger;
             ProductService = productService;
         }
+        // Initializes the page
         public void OnGet()
         {
+            // Generates the list of products from the json file
             Products = ProductService.GetProducts();
         }
     }
