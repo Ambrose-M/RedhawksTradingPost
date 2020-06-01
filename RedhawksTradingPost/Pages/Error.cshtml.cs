0a9860df771b3f12fb33d06ec74d10cbf3064bc4
Hung Huynh <huynhh1@seattleu.edu>
2020-06-01 16:52:53 -0700

    Add comment in Error file

--- RedhawksTradingPost/Pages/Error.cshtml.cs
+++ RedhawksTradingPost/Pages/Error.cshtml.cs
@@ -12,17 +12,24 @@ namespace RedhawksTradingPost.Pages
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        //error ID
         public string RequestId { get; set; }
 
+        //request ID
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        //logger
         private readonly ILogger<ErrorModel> _logger;
 
+        //Constructor
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Get RequestID
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
