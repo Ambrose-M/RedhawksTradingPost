2a24a03cb2872a353074c03a41cb4faacba3a5f5
ambrose-m <ambrose907@gmail.com>
2020-06-02 19:43:38 -0700

    Moved Javascript from ItemPage to site.js
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/xrQ5IBETSkSsKuWge-hX4GQAAHc-?Type=TaskLink&Channel=Link&CreatedTime=637267489480770000

--- RedhawksTradingPost/wwwroot/js/site.js
+++ RedhawksTradingPost/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Opens Make an Offer pop-up form
+function openForm()
+{
+    document.getElementById("myForm").style.display = "block";
+}
 
-// Write your Javascript code.
+
+//Closes Make an Offer pop-up form
+function closeForm()
+{
+    document.getElementById("myForm").style.display = "none";
+}
